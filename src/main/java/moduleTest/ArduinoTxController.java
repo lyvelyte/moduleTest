ea24b7cf389ca2fb21bb55ea3c9c78f208f72fc9
Brendan J. Alvey <lyvelyve@gmail.com>
2020-03-01 14:21:30 -0600

    Discovered issue had to do with powering up the arduino while receiving data.

--- src/main/java/moduleTest/ArduinoTxController.java
+++ src/main/java/moduleTest/ArduinoTxController.java
@@ -25,8 +25,9 @@ public class ArduinoTxController {
     private boolean isShuttingDown = false;
     private static final Random random = new Random();
 
-    private static final int nFrames = 2000;
+    private static final int nFrames = 200000;
     private static boolean swtichFlip = false;
+    private static boolean swtichFlip2 = false;
 
     // Data Format
     // data[0] = startMarker
@@ -68,7 +69,7 @@ public class ArduinoTxController {
     // data[36] = endMarker
 
     private byte getColor(){
-        double z = (frameNumber)/(4d*nFrames);
+        double z = (1000d*frameNumber)/(4d*nFrames);
         double x = Math.sin(2d*Math.PI*z);
         double xAmp = (0.5d*x+0.5d)*128d;
         int a = (int) xAmp;
@@ -78,76 +79,110 @@ public class ArduinoTxController {
     }
 
     private byte[] getBadData(){
-        byte[] data = new byte[DATA_SIZE-5];
+        byte[] data = new byte[9];
         data[0] = (byte) startMarker;
-        data[1] = 0;
-        data[2] = 64;
-        if(frameNumber%3 == 0){
-            data[3] = getColor();
-        }else if(frameNumber%2 ==0){
-            data[4] = getColor();
-        }else{
-            data[5] = getColor();
-        }
-        data[data.length - 1] = (byte) endMarker;
+        data[data.length-1] = (byte) endMarker;
         return data;
     }
 
+    public static float[] HSVtoRGB(float h, float s, float v)
+    {
+        // H is given on [0->6] or -1. S and V are given on [0->1].
+        // RGB are each returned on [0->1].
+        float m, n, f;
+        int i;
+
+        float[] hsv = new float[3];
+        float[] rgb = new float[3];
+
+        hsv[0] = h;
+        hsv[1] = s;
+        hsv[2] = v;
+
+        if (hsv[0] == -1)
+        {
+            rgb[0] = rgb[1] = rgb[2] = hsv[2];
+            return rgb;
+        }
+        i = (int) (Math.floor(hsv[0]));
+        f = hsv[0] - i;
+        if (i % 2 == 0)
+        {
+            f = 1 - f; // if i is even
+        }
+        m = hsv[2] * (1 - hsv[1]);
+        n = hsv[2] * (1 - hsv[1] * f);
+        switch (i)
+        {
+            case 6:
+            case 0:
+                rgb[0] = hsv[2];
+                rgb[1] = n;
+                rgb[2] = m;
+                break;
+            case 1:
+                rgb[0] = n;
+                rgb[1] = hsv[2];
+                rgb[2] = m;
+                break;
+            case 2:
+                rgb[0] = m;
+                rgb[1] = hsv[2];
+                rgb[2] = n;
+                break;
+            case 3:
+                rgb[0] = m;
+                rgb[1] = n;
+                rgb[2] = hsv[2];
+                break;
+            case 4:
+                rgb[0] = n;
+                rgb[1] = m;
+                rgb[2] = hsv[2];
+                break;
+            case 5:
+                rgb[0] = hsv[2];
+                rgb[1] = m;
+                rgb[2] = n;
+                break;
+        }
+
+        return rgb;
+
+    }
+
     private byte[] getData() {
-        byte[] data = new byte[DATA_SIZE];
-        data[0] = (byte) startMarker;
-        data[1] = 0;
-        data[2] = 64;
         if(frameNumber % 30 == 0){
             if(swtichFlip){
                 swtichFlip = false;
             }else{
                 swtichFlip = true;
             }
+        }else if(frameNumber %1000 == 0){
+            double p = ((double) frameNumber) / ((double) nFrames);
+            LOGGER.info("frameNumber = {}/{}={}", frameNumber, nFrames, p);
+        }else if(frameNumber %200 == 0){
+            if(swtichFlip2){
+                swtichFlip2= false;
+            }else{
+                swtichFlip2 = true;
+            }
+            LOGGER.info("SwitchFlip 2 == {}", swtichFlip2);
         }
+
+        float[] curColor = HSVtoRGB(((float) getColor())/127f, 1f, 1f);
+        byte[] data = new byte[DATA_SIZE];
+        data[0] = (byte) startMarker;
+        data[2] = 64;
         if(swtichFlip){
-            data[3] = (byte) 128;
-            data[4] = (byte) 128;
-            data[5] = (byte) 128;
-        }else{
-            data[3] = 0;
-            data[4] = 0;
-            data[5] = 0;
+            data[3] = (byte) (curColor[0]*253);
+            data[4] = (byte) (curColor[1]*253);
+            data[5] = (byte) (curColor[2]*253);
         }
         data[6] = 1;
-        data[7] = 0;
-        data[8] = 0;
-        data[9] = 0;
-        data[10] = 0;
-        data[11] = 0;
-        data[12] = 0;
-        data[13] = 0;
-        data[14] = 0;
-        data[15] = 0;
-        data[16] = 0;
-        data[17] = 0;
-        data[18] = 0;
-        data[19] = 0;
-        data[20] = 0;
-        data[21] = 0;
-        data[22] = 0;
-        data[23] = 0;
-        data[24] = 0;
-        data[25] = 0;
-        data[26] = 0;
-        data[27] = 0;
-        data[28] = 0;
-        data[29] = 0;
-        data[30] = 0;
-        data[31] = 0;
-        data[32] = 0;
-        data[33] = 0;
-        data[34] = 0;
-        if(!swtichFlip){
-            data[35] = 0;
-        }else{
-            refreshChecksum(data);
-            data[36] = (byte) endMarker;
+        data[36] = (byte) endMarker;
+        if(swtichFlip2){
+            data = getBadData();
         }
         return data;
     }
@@ -195,16 +230,7 @@ public class ArduinoTxController {
             if(frameNumber > nFrames){
                 cleanUp();
             }else{
-//                if(frameNumber > 30 && frameNumber < 40){
-//                    sendDataToTx(getBadData());
-//                }else{
-                    sendDataToTx(getData());
-//                }
-//                try{
-//                    Thread.sleep(50);
-//                }catch(Exception ignored){
-
-//                }
+                sendDataToTx(getData());
             }
         }
     }
